33267bc0b251047d46fe7c1d27f3b093daa771d7
Dinesh <Dineshhire4@gmail.com>
2022-01-20 23:09:48 +0530

    Initial commit

---
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form',
+  templateUrl: './template-driven-form.component.html',
+  styleUrls: ['./template-driven-form.component.css']
+})
+
+export class TemplateDrivenFormComponent implements OnInit {
+getUseritem(item:any)
+  {
+    console.log(item);
+    
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+}
